b464f2a423522c3377ddc144ed8f2449d42367b6
Lacc Riophae <iruif.cn@gmail.com>
2013-12-05 18:06:38 +0800

    细节更新

--- js/popup.js
+++ js/popup.js
@@ -1348,7 +1348,8 @@ tl_model.initialize = function() {
 				drawAttention();
 			pre_count.timeline = tl.buffered.length;
 		}
-		if (! is_focused || $main[0].scrollTop) return;
+		if (! is_focused || $main[0].scrollTop > $body.height / 2)
+			return;
 		var buffered = tl.buffered;
 		tl.buffered = [];
 		if (! tl.tweets.length) {
