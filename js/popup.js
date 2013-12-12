8f02d603b1b464e5dcf20c839ef8b409c6352b85
Lacc Riophae <iruif.cn@gmail.com>
2013-12-12 23:57:27 +0800

    修正字体错位

--- js/popup.js
+++ js/popup.js
@@ -619,6 +619,8 @@ function initMainUI() {
 		$('html').attr('platform', 'linux');
 	} else if (PREFiX.is_mac) {
 		$('html').attr('platform', 'mac');
+	} else if (is_windows) {
+		$('html').attr('platform', 'win');
 	}
 
 	var ratio = +PREFiX.settings.current.zoomRatio;
@@ -641,6 +643,12 @@ function initMainUI() {
 			$('<link />').
 			prop('rel', 'stylesheet').
 			prop('href', 'css/font-fix-2.css').
+			appendTo('head');
+		}
+		if (ratio === 1.5) {
+			$('<link />').
+			prop('rel', 'stylesheet').
+			prop('href', 'css/font-fix-3.css').
 			appendTo('head');
 		}
 	}
